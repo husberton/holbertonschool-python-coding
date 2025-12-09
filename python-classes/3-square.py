6f743b79abcefb6282afb511c30343f3c9bed44b
Huseyn Huseynli <12683@holbertonstudents.com>
2025-12-09 19:09:43 +0000

    Task 3

--- python-classes/3-square.py
+++ python-classes/3-square.py
@@ -1,22 +1,31 @@
 #!/usr/bin/python3
-""" python3 -c 'print(__import__("my_module").__doc__)' """
+
+"""
+Docstring for 3-square.py
+"""
 
 
 class Square:
-    """ python3 -c 'print(__import__("my_module").MyClass.__doc__)' """
+
+    """
+    Docstring for Square
+    """
+
     def __init__(self, size=0):
         if not isinstance(size, int):
             raise TypeError("size must be an integer")
-        if size < 0:
+        elif size < 0:
             raise ValueError("size must be >= 0")
         self.__size = size
 
     @property
     def size(self):
+        if not isinstance(self.__size, int):
+            raise Exception("size must be an integer")
         return self.__size
 
     @size.setter
-    def size(self, value):
+    def size(self, size):
         if not isinstance(size, int):
             raise TypeError("size must be an integer")
         elif size < 0:
@@ -24,4 +33,8 @@ class Square:
         self.__size = size
 
     def area(self):
-        return self.__size ** 2
+        if not isinstance(self.__size, int):
+            raise TypeError("size must be an integer")
+        elif self.__size < 0:
+            raise ValueError("size must be >= 0")
+        return self.__size * self.__size
