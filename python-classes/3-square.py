2357fe2cb1cd3570b5041a1f6c15f20fccb04f4e
Huseyn Huseynli <12683@holbertonstudents.com>
2025-12-09 19:07:08 +0000

    Task 3

--- python-classes/3-square.py
+++ python-classes/3-square.py
@@ -17,7 +17,11 @@ class Square:
 
     @size.setter
     def size(self, value):
-        self.__size = value
+        if not isinstance(size, int):
+            raise TypeError("size must be an integer")
+        elif size < 0:
+            raise ValueError("size must be >= 0")
+        self.__size = size
 
     def area(self):
         return self.__size ** 2
