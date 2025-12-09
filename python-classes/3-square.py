fc02374c1c00451e795c06a71372e2ab728b8363
Huseyn Huseynli <12683@holbertonstudents.com>
2025-12-09 18:58:35 +0000

    Task 3

---
+++ python-classes/3-square.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+""" python3 -c 'print(__import__("my_module").__doc__)' """
+
+
+class Square:
+    """ python3 -c 'print(__import__("my_module").MyClass.__doc__)' """
+    def __init__(self, size=0):
+        if not isinstance(size, int):
+            raise TypeError("size must be an integer")
+        if size < 0:
+            raise ValueError("size must be >= 0")
+        self.__size = size
+
+    def size(self):
+        return self.__size
+
+    def size(self, value):
+        self.__size = value
+
+    def area(self):
+        return self.__size ** 2
