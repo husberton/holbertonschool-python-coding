da11cb7f1150c788d4506195c499834e92e50da7
Huseyn Huseynli <12683@holbertonstudents.com>
2025-12-09 18:26:56 +0000

    Task 0: Square

--- python-classes/0-square.py
+++ python-classes/0-square.py
@@ -1,5 +1,6 @@
 #!/usr/bin/python3
+""" python3 -c 'print(__import__("my_module").__doc__)' """
 class Square:
-    __size = None
+    """ python3 -c 'print(__import__("my_module").MyClass.__doc__)' """
     def __init__(self, size):
         self.__size = size
